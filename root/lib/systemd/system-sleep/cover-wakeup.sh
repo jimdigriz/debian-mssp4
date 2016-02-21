0ee2ebc6a087fa80d3fa818466874983e01cb82d
Alexander Clouter <alex@digriz.org.uk>
2016-02-21 13:04:05 +0000

    make the cover event wake logic a bit more intelligent

--- root/lib/systemd/system-sleep/cover-wakeup.sh
+++ root/lib/systemd/system-sleep/cover-wakeup.sh
@@ -6,9 +6,15 @@ set -eu
 
 case "$1" in
 pre)
-	grep -q 'XHC.*enabled' /proc/acpi/wakeup || echo XHC > /proc/acpi/wakeup
-	echo enabled > /sys/bus/usb/devices/usb1/power/wakeup
-	test -d /sys/bus/usb/devices/usb1/1-7 && echo enabled > /sys/bus/usb/devices/usb1/1-7/power/wakeup
+	# if the cover is plugged in, wake on any cover events,
+	# otherwise make the user use the power button instead
+	if test -d /sys/bus/usb/devices/usb1/1-7; then
+		grep -q 'XHC.*enabled' /proc/acpi/wakeup || echo XHC > /proc/acpi/wakeup
+		echo enabled > /sys/bus/usb/devices/usb1/power/wakeup
+		echo enabled > /sys/bus/usb/devices/usb1/1-7/power/wakeup
+	else
+		grep -q 'XHC.*enabled' /proc/acpi/wakeup && echo XHC > /proc/acpi/wakeup
+	fi
 	;;
 post)
 	# stops additional lid events coming in that on resume put the laptop back to sleep
